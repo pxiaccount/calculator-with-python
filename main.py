1ba7461a91cb8ecf46556dc3526263336d4662ae
pxiaccount <kengunpai8855@gmail.com>
2025-01-22 11:33:09 +0700

    Fix bugs

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ display_text.set("0")
 display_ui = Label(display_frame, textvariable=display_text, height=2, width=10, relief="sunken", anchor="e")
 display_ui.pack(fill="x")
 
-operator_text = "+-×÷%"
+operator_text = "+-*/%"
 
 buttons = [
     ("C", 1, 0), ("←", 1, 1), ("√x", 1, 2), ("+", 1, 3),
     ("7", 2, 0), ("8", 2, 1), ("9", 2, 2), ("-", 2, 3),
-    ("4", 3, 0), ("5", 3, 1), ("6", 3, 2), ("×", 3, 3),
-    ("1", 4, 0), ("2", 4, 1), ("3", 4, 2), ("÷", 4, 3),
+    ("4", 3, 0), ("5", 3, 1), ("6", 3, 2), ("*", 3, 3),
+    ("1", 4, 0), ("2", 4, 1), ("3", 4, 2), ("/", 4, 3),
     ("0", 5, 0), (".", 5, 1), ("%", 5, 2), ("=", 5, 3),
 ]
 
@@ -33,10 +33,8 @@ def update(x):
 def operation(x):
     current = display_text.get()
     
-    if current[-1:] not in operator_text:
+    if current[-1] != x and current[-1] not in operator_text:
         display_text.set(current+x)
-    else:
-        return
 
 for (text, row, column) in buttons:
     if text in operator_text:
